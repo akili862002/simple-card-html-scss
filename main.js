58cc0aefd3e813702e7ad48fb8adc0417da729e7
“Surtin” <surtin@usumsoftware.com>
2021-09-18 08:56:34 +0700

    add responsive

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import './styles/index.scss'
+
+const card = document.querySelector(".card__container");
+
+console.log(card.offsetWidth);
+
+window.addEventListener("resize", () => {
+    if (card.offsetWidth < 530) {
+        card.classList.add("card__container--small");
+    } else {
+        card.classList.remove("card__container--small");
+    }
+})
